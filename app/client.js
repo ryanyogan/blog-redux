41221c1be87a0436ac89bcb5dedee221dddb9c1b
Ryan Yogan <ryanyogan@gmail.com>
2015-09-14 11:51:24 -0500

    Added bootstrap wrapper and basic state hydration

---
+++ app/client.js
@@ -0,0 +1,17 @@
+/* eslint-env browser */
+/* global process */
+
+import 'babel/polyfill';
+import React from 'react';
+import BrowserHistory from 'react-router/lib/BrowserHistory';
+import HashHistory from 'react-router/lib/HashHistory';
+import Root from './Root';
+
+const history = (process.env.NODE_ENV === 'production')
+  ? new BrowserHistory()
+  : new HashHistory();
+
+React.render(
+  <Root {...{ history }} />,
+  document.geElementById('app')
+);
