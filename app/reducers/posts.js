2a9117867b01753eaff8b3dc3c1aa30a78f9a71f
Ryan Yogan <ryanyogan@gmail.com>
2015-09-15 09:02:40 -0500

    added post recuders

---
+++ app/reducers/posts.js
@@ -0,0 +1,31 @@
+import {
+  FETCH_POST_SUCCESS,
+  FETCH_POSTS_SUCCESS,
+  SAVE_POST_SUCCESS
+} from '../constants/actions';
+
+export default (state = { list: [], items: {} }, actions) => {
+  switch (action.type) {
+    case FETCH_POSTS_SUCCESS:
+      const list = action.posts.map(item => item.id);
+      const items = {};
+
+      action.posts.forEach(post => { items[post.id] = post; });
+
+      return { list, items };
+
+    case SAVE_POST_SUCCESS:
+    case FETCH_POST_SUCCESS:
+      return {
+        items: {
+          ...state.items,
+          [action.post.id]: action.post
+        },
+
+        list: state.list
+      };
+
+    default:
+      return state;
+  }
+};
