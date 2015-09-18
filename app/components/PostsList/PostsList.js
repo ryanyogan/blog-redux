a7fdf84562d4515146e79c0e5a804334fbccbfc9
Ryan Yogan <ryanyogan@gmail.com>
2015-09-18 11:20:32 -0500

    Added postslist component

---
+++ app/components/PostsList/PostsList.js
@@ -0,0 +1,34 @@
+import styles from './styles.styl';
+
+import React, { PropTypes } from 'react';
+import moment from 'moment';
+import { Link } from 'react-router';
+import CSSModules from 'react-css-modules';
+
+@CSSModules(styles)
+export default class PostsList extends React.Component {
+  static propTypes = {
+    posts: PropTypes.array
+  }
+
+  render() {
+    return (
+      <div styleName='wrapper'>
+        {this.props.posts.filter(item => item.published)
+          .map(post => {
+            const publishedAt = moment(new Date(post.date));
+
+            return (
+              <div key={post.id}>
+                <Link to={`/posts/${post.id}`}>
+                  <h2 className='post-header-link'>{post.title}</h2>
+                </Link>
+
+                <small>{publishedAt.format('dddd, h:mm a')}</small>
+              </div>
+            );
+        })}
+      </div>
+    );
+  }
+}
